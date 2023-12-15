fed21a4977e6f7ecdafd65eee97841bfcf5da76e
Kyndal LoCascio <KyndalLoCascio@gmail.com>
2023-12-15 13:32:59 -0500

    python function review

--- Main.py
+++ Main.py
@@ -45,7 +45,7 @@
 #     print(division_instance.integer_results)
 #     print(division_instance.float_results)
 
-# # INTRO
+# # INTRO https://github.com/SEIR-0911/u4_lesson_python_intro/tree/main
 # # Execute Python scripts
 # # Define and use variables
 # # Create comments
@@ -82,14 +82,58 @@
 
 # print(a//b, a%b)
 
-hours_in_day = 24
-days_in_year = 365
+# hours_in_day = 24
+# days_in_year = 365
 
-# f-string interpolation
-msg = f'There are {hours_in_day} hours in a day, and {days_in_year} days in a year. The total hours in a year is {hours_in_day * days_in_year} hours.'
+# # f-string interpolation
+# msg = f'There are {hours_in_day} hours in a day, and {days_in_year} days in a year. The total hours in a year is {hours_in_day * days_in_year} hours.'
 
-# string format method
-template = "There are {} hours in a day, and {} days in a year."
-print(template.format(24, 365))
+# # string format method
+# template = "There are {} hours in a day, and {} days in a year."
+# print(template.format(24, 365))
 
-print(msg)
+# print(msg)
+
+# # To convert one data type into another, Python provides us with several global functions or predefined classes to do so:
+# # str(item)        # Converts item to a string
+# # int(item, base)  # Converts the provided item to an integer with the provided base
+# # float(item)      # Converts the item to a floating-point number
+# # hex(int)         # Converts an integer to a hexadecimal STRING
+# # oct(int)         # Converts an integer to an octal STRING
+# # tuple(item)      # Converts item to a tuple
+# # list(item)       # Converts item to a list
+# # dict(item)       # Converts item to a dictionary
+
+# # PYTHON FUNCTIONS https://github.com/SEIR-0911/u4_lesson_python_functions
+# # Compare Python to JavaScript functions
+# # Define functions in Python
+# # Invoke functions in Python
+
+# def say_hello():
+#     print('Hello!')
+
+# say_hello()
+
+# def do_math():
+#     my_age = 30
+#     weeks_in_year = 52
+#     my_age_in_weeks = my_age * weeks_in_year
+#     print(f'I am {my_age_in_weeks} weeks old.')
+
+# do_math()
+
+# # arguments/parameters
+
+# def add_nums(num1, num2):
+#     print(num1 + num2)
+
+# add_nums(30, 40)
+
+# # Lambda, similar to Arrow Functions in JS
+
+# nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 28]
+# odds = list( filter(lambda num: num % 2, nums) )
+# evens = list ( filter(lambda num: num % 2 == 0, nums) )
+# print(odds)
+# print(evens)
+
